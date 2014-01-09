982e95d536b77c85a24469d94ba870fe7fe5340c
andy rose <thatandyrose@gmail.com>
2014-01-09 19:33:06 +0800

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tlrl::Application.config.secret_token = '6f12c97a7f9061771037039a138254b258e638fa7e103cf02e23bd4cdd4ec6a5a36597823879080e2e626d09c10e86df41659de99dd3ba70db0db00f3553611c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Tlrl::Application.config.session_store :cookie_store, key: '_tlrl_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Tlrl::Application.config.session_store :active_record_store
